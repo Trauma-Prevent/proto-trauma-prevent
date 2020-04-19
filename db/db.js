c25187339364de75f1cd366bb4cb0abd2652518b
Guillaume Ferron <guillaume.ferron@gmail.com>
2020-04-19 18:22:56 +0200

    add (dynamic) recording of form1 in database

--- db/db.js
+++ db/db.js
@@ -1,12 +1,16 @@
 
-SALTA=1
+const moment = require('moment');
 const crypto = require('crypto');
 const Sequelize = require('sequelize');
 // const sequelize = new Sequelize(process.env.DATABASE_URI);
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:  'postgres'
+    dialect:  'postgres',
+    //logging: false
 })
 const db = require('./db.model')
+
+//        const Users = db.users
+//        Users.sync({ alter: true })
 
 const getHashedPassword = (password) => {
     const sha256 = crypto.createHash('sha256');
@@ -74,6 +78,53 @@ module.exports = {
                             const hashedPassword = getHashedPassword(clearpassword + user.salt)
                             if (user.password == hashedPassword) {
                                 resolve()
+                            } else {
+                                reject()
+                            }
+                        } else {
+                            reject()
+                        }
+                    })
+                    //.catch(error => res.status(400).json({error}));;
+                }, {
+                    // options
+                })
+                .catch(err => {
+                    reject();
+                    console.error('Unable to connect to the database:', err);
+                })
+        })
+    },
+    saveForm1: function(email, data) {
+
+        delete data['submit'] //could be better
+
+        return new Promise(function(resolve, reject) {
+            sequelize
+                .authenticate()
+                .then(() => {
+
+                    db.users.findAll({
+                        where: {
+                            email: email
+                        }
+                    }).then((results) => {            
+                        const user = results[0]
+                        if(user) {
+                            let user_id = user.id
+                            let date = moment()
+                            if (user_id) {
+                                const Form1 = db.form1(data)
+                                Form1.sync({ alter: true })
+                                data['user_id'] = user_id
+                                data['date'] = date
+
+                                Form1.create(data).then((result) => {
+                                    resolve()
+                                }, () => {
+                                    reject()
+                                });
+
                             } else {
                                 reject()
                             }
