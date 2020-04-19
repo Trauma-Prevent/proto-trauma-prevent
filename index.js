1cb173e6729f4e1cbd6906d25c1d7a10ae821b4c
Guillaume Ferron <guillaume.ferron@gmail.com>
2020-04-19 11:08:08 +0200

    add auth

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const app = express();
-
-const port = process.env.PORT || 80
-
 const crypto = require('crypto');
+const db = require('./db/db.js')
 
-const getHashedPassword = (password) => {
-    const sha256 = crypto.createHash('sha256');
-    const hash = sha256.update(password).digest('base64');
-    return hash;
-}
+const app = express();
+const port = process.env.PORT || 80
 
 const generateAuthToken = () => {
     return crypto.randomBytes(30).toString('hex');
@@ -20,8 +14,11 @@ const generateAuthToken = () => {
 const authTokens = {};
 
 
-app.get("/", (req, res, next) => {
-    res.sendFile('index.html', { root: __dirname });
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.get("/", (req, res, next) => { // should stay here in current state
+    res.status(200).sendFile('index.html', { root: __dirname });
 });
 
 app.use('/static/', express.static(__dirname+'/static'));
@@ -42,92 +39,113 @@ app.use((req, res, next) => {
 });
 
 app.get("/login", (req, res) =>{
-    res.sendFile('pages/login.html', { root: __dirname });
+    res.status(200).sendFile('pages/login.html', { root: __dirname });
 })
+
+app.get("/register", (req, res) =>{
+    res.status(200).sendFile('pages/register.html', { root: __dirname });
+})
+
+app.post("/register", (req, res) =>{
+    const { email, password } = req.body
+    db.createUser(email, password)
+    .then(() => {
+        res.status(200).sendFile('pages/login.html', { root: __dirname });
+    }, () => {
+        res.status(400).sendFile('pages/register.html', { root: __dirname });
+    });
+});
 
 app.post("/login", (req, res) =>{
     const { email, password } = req.body;
-    const hashedPassword = getHashedPassword(password);
 
-    // This is just a Stub
-    // FIXME: replace this with DB call (using Sequelize)
-    const user = {email: email, password: hashedPassword};
-    if (user) {
+    db.verifyUser(email, password)
+    .then(() => {
         const authToken = generateAuthToken();
-
-
         // Store authentication token
+        const user = {email: email};
         authTokens[authToken] = user;
-
         // Setting the auth token in cookies
         res.cookie('AuthToken', authToken);
-
         // Redirect user to the protected page
-        res.redirect('/page1');
-    } else {
-        res.render('login', {
-            message: 'Invalid username or password',
-            messageClass: 'alert-danger'
-        });
-    }
+        res.status(200).redirect('/page1');
+    }, () => {
+        res.status(400).sendFile('pages/login.html', { root: __dirname });
+        // FIX ME : need pug-boostrap or ejs (or similar)
+        //res.render('login', {
+        //    message: 'Invalid username or password',
+        //    messageClass: 'alert-danger'
+        //});
+    });
 })
 
 app.get("/page1", (req, res, next) => {
-    if(!req.user) res.redirect("/login");
-
-    res.sendFile('pages/form1.html', { root: __dirname });
+    if(!req.user) {
+        res.status(401).redirect("/login");
+    } else {
+        res.status(200).sendFile('pages/form1.html', { root: __dirname });
+    }
 });
 
 app.post('/page1', (req, res) => {
-    if(!req.user) res.redirect("/login");
-
-    console.log('Got body:', req.body);
-    res.redirect('/page2')
+    if(!req.user) {
+        res.status(401).redirect("/login");
+    } else {
+        res.status(200).redirect('/page2')
+    }
 });
 
 app.get("/page2", (req, res, next) => {
-    if(!req.user) res.redirect("/login");
-
-    let rand = Math.floor(Math.random() * (2 - 0 + 1));
-    let file = 'Error'
-    switch(rand) {
-        case 0:
-            file = 'pages/support.html'
-            break
-        case 1:
-            file = 'pages/tau.html'
-            break
-        default:
-            file = 'pages/tetris.html'
-            break
+    if(!req.user) {
+        res.status(401).redirect("/login");
+    } else {
+        let rand = Math.floor(Math.random() * (2 - 0 + 1));
+        let file = 'Error'
+        switch(rand) {
+            case 0:
+                file = 'pages/support.html'
+                break
+            case 1:
+                file = 'pages/tau.html'
+                break
+            default:
+                file = 'pages/tetris.html'
+                break
+        }
+        res.status(200).sendFile(file, { root: __dirname });
     }
-    res.sendFile(file, { root: __dirname });
 });
 
 app.post('/page2', (req, res) => {
-    if(!req.user) res.redirect("/login");
-
-    console.log('Got body:', req.body);
-    res.redirect('/page3')
+    if(!req.user) {
+        res.status(401).redirect("/login");
+    } else {
+        res.status(200).redirect('/page3')
+    }
 });
 
 app.get("/page3", (req, res, next) => {
-    if(!req.user) res.redirect("/login");
-
-    res.sendFile('pages/form2.html', { root: __dirname });
+    if(!req.user) {
+        res.status(401).redirect("/login");
+    } else {
+        res.status(200).sendFile('pages/form2.html', { root: __dirname });
+    }
 });
 
 app.post('/page3', (req, res) => {
-    if(!req.user) res.redirect("/login");
-
-    console.log('Got body:', req.body);
-    res.redirect('/page4')
+    if(!req.user) {
+        res.status(401).redirect("/login");
+    } else {
+        res.status(200).redirect('/page4')
+    }
 });
 
 app.get("/page4", (req, res, next) => {
-    if(!req.user) res.redirect("/login");
-
-    res.sendFile('pages/end.html', { root: __dirname });
+    if(!req.user) {
+        res.status(401).redirect("/login");
+    } else {
+        res.status(200).sendFile('pages/end.html', { root: __dirname });
+    }
 });
 
 app.listen(port, () => {
